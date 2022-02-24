ea24d923af5ffc327bca3b71cfdf27dee6299c63
SriHarsha Ghanta <harshaghanta@gmail.com>
2022-02-25 00:13:33 +0530

    Added IntegrationLogEntry Entity

---
+++ src/Services/catalogapi/src/main/java/com/eshoponcontainers/catalogapi/config/EntityScanConfig.java
@@ -0,0 +1,10 @@
+package com.eshoponcontainers.catalogapi.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EntityScan(basePackages = {"com.eshoponcontainers.catalogapi.entities", "com.eshoponcontainers.integration.entities"})
+public class EntityScanConfig {
+    
+}
