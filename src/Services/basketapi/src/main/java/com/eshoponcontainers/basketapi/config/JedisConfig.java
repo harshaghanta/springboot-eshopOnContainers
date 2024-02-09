0c8ab77ac9ac472b303fcf3bfe798823e87e1108
SriHarsha Ghanta <harshaghanta@gmail.com>
2024-02-09 09:34:58 +0530

    jedis compat issue with spring boot & added request scope

--- src/Services/basketapi/src/main/java/com/eshoponcontainers/basketapi/config/JedisConfig.java
+++ src/Services/basketapi/src/main/java/com/eshoponcontainers/basketapi/config/JedisConfig.java
@@ -1,11 +1,17 @@
 package com.eshoponcontainers.basketapi.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 @Slf4j
@@ -13,12 +19,34 @@ public class JedisConfig {
 
     @Value("${basketdata.host}")
     private String host;
-    
+
     @Bean
-    public Jedis jedis() {
+    @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public Jedis jedis(JedisPool jedisPool) {
         log.info("Printing basketdata host: {}", host);
 
-        var jedis = new Jedis(host, 6379);
-        return jedis;
+        return jedisPool.getResource();
     }
+
+    @Bean
+    public JedisPoolConfig buildPoolConfig() {
+        final JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128);
+        poolConfig.setMaxIdle(128);
+        poolConfig.setMinIdle(16);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
+        poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
+        poolConfig.setNumTestsPerEvictionRun(3);
+        poolConfig.setBlockWhenExhausted(true);
+        return poolConfig;
+    }
+
+    @Bean
+    public JedisPool jedisPool(JedisPoolConfig poolConfig) {
+        return new JedisPool(poolConfig, host);
+    }
+
 }
