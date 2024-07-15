6b0e8d2e1023e18037f8eadab5e2b45e26290dcf
SriHarsha Ghanta <harshaghanta@gmail.com>
2024-07-16 00:21:05 +0530

    Initial changes for k8s

---
+++ deploy/k8s/helm/deploy_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+app_name="eshop"
+chart='rabbitmq'
+namespace="eshop"
+dns=''
+# ingress_values_file="ingress_values.yaml"
+image_tag='latest'
+imagePullPolicy='IfNotPresent'
+
+helm install "$app_name-$chart" --namespace $namespace --set "ingress.hosts={$dns}" --values app.yaml --values inf.yaml --set app.name=$app_name --set inf.k8s.dns=$dns --set image.tab=$image_tag --set image.pullPolicy=$imagePullPolicy $chart 
+# --dry-run --debug
