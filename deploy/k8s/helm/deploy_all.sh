64c69d0ca5101a462e6f4e4752c75a06e85672aa
SriHarsha Ghanta <harshaghanta@gmail.com>
2024-08-15 13:25:12 +0530

    Fixed the basket, catalog and keycloak related charts

--- deploy/k8s/helm/deploy_all.sh
+++ deploy/k8s/helm/deploy_all.sh
@@ -2,9 +2,10 @@
 app_name="springeshop"
 chart='webspa'
 namespace="springeshop"
-dns=''
+dns='localhost'
 # ingress_values_file="ingress_values.yaml"
 image_tag='latest'
 imagePullPolicy='IfNotPresent'
 
-helm install "$app_name-$chart" --namespace $namespace --set "ingress.hosts={$dns}" --values app.yaml --values inf.yaml --set app.name=$app_name --set inf.k8s.dns=$dns --set image.tab=$image_tag --set image.pullPolicy=$imagePullPolicy $chart #--dry-run --debug
+helm install "$app_name-$chart" --namespace $namespace --set "ingress.hosts={$dns}" --values app.yaml --values inf.yaml --set app.name=$app_name --set inf.k8s.dns=$dns $chart #--dry-run --debug
+#--set image.tag=$image_tag --set image.pullPolicy=$imagePullPolicy 
