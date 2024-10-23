b59ec95d39db2e878a7c16c962eb744b2053d4d5
SriHarsha Ghanta <harshaghanta@gmail.com>
2024-10-24 01:59:31 +0530

    Refactor CORS configuration to use dynamic allowed origin

--- src/Services/catalogapi/src/main/java/com/eshoponcontainers/catalogapi/config/WebConfig.java
+++ src/Services/catalogapi/src/main/java/com/eshoponcontainers/catalogapi/config/WebConfig.java
@@ -8,15 +8,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableWebMvc
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${allowedCorsOrigin}")
     private String allowedCorsOrigin;
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {    
+        log.info("Adding CORS mappings for origin:", allowedCorsOrigin);
         registry.addMapping("/**").allowedOrigins(allowedCorsOrigin).allowedMethods("*")
                 .allowedHeaders("*");
     }
