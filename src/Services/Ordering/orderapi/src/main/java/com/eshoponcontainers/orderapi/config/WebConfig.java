75a88f1d74d89397c965f44f470f2b2b9e28786b
SriHarsha Ghanta <harshaghanta@gmail.com>
2024-10-25 00:08:49 +0530

    removed hardcoded cors origin & pulled it from config

--- src/Services/Ordering/orderapi/src/main/java/com/eshoponcontainers/orderapi/config/WebConfig.java
+++ src/Services/Ordering/orderapi/src/main/java/com/eshoponcontainers/orderapi/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.eshoponcontainers.orderapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,13 +8,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableWebMvc
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${allowedCorsOrigin}")
+    private String allowedCorsOrigin;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://host.docker.internal:8080").allowedMethods("*")
+        log.info("Adding CORS mappings for origin: {}", allowedCorsOrigin);
+        registry.addMapping("/**").allowedOrigins(allowedCorsOrigin).allowedMethods("*")
                 .allowedHeaders("*").allowCredentials(true);
     }
 
